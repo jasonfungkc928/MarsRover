13583b45efdb8430ecdd046f9da52c5c5154f41d
Z5193621 <bichtuyennt15@gmail.com>
2020-04-26 11:22:03 +1000

    make changes for the profile screen

--- app/src/main/java/com/GroupAssignment/marsrover/ProfileScreen.java
+++ app/src/main/java/com/GroupAssignment/marsrover/ProfileScreen.java
@@ -10,40 +10,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import static com.GroupAssignment.marsrover.HomeScreen.userScore;
 
 public class ProfileScreen extends AppCompatActivity {
-
-    private TextView name, score, medal, lesson1, lesson2, lesson3, lesson4;
-    private ImageView profilepic, medal1, medal2, medal3, medal4;
+    TextView notice1, notice2, stage0, stage1, stage2, stage3;
+    ImageView medal1, medal2, medal3, medal4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profilescreen);
-        name = (TextView) findViewById(R.id.name);
-        score = (TextView) findViewById(R.id.score);
-        medal = (TextView) findViewById(R.id.medal);
-        lesson1 = (TextView) findViewById(R.id.lesson1);
-        lesson2 = (TextView) findViewById(R.id.lesson2);
-        lesson3 = (TextView) findViewById(R.id.lesson3);
-        lesson4 = (TextView) findViewById(R.id.lesson4);
-        profilepic = (ImageView) findViewById(R.id.profilepic);
-        medal1 = (ImageView) findViewById(R.id.medal1);
-        medal2 = (ImageView) findViewById(R.id.medal2);
-        medal3 = (ImageView) findViewById(R.id.medal3);
-        medal4 = (ImageView) findViewById(R.id.medal4);
+        notice1 = findViewById(R.id.notice1);
+        notice2 = findViewById(R.id.notice2);
+        stage0 = findViewById(R.id.stage0Text);
+        stage1 = findViewById(R.id.stage1Text);
+        stage2 = findViewById(R.id.stage2Text);
+        stage3 = findViewById(R.id.stage3Text);
+        medal1 = findViewById(R.id.medal1);
+        medal2 = findViewById(R.id.medal2);
+        medal3 = findViewById(R.id.medal3);
+        medal4 = findViewById(R.id.medal4);
 
-
-        name.setText("Student");
-        //int totalScore = sum of the score of every game
-        //score.setText("Total score:" + Integer.ValueOf(totalScore)
-
-        if (userScore>=3)
+        if (userScore >= 3) {
             medal1.setVisibility(View.VISIBLE);
-        if (userScore>=6)
-            medal1.setVisibility(View.VISIBLE);
-        if (userScore>=9)
-            medal1.setVisibility(View.VISIBLE);
-        if (userScore>=12)
-            medal1.setVisibility(View.VISIBLE);
+            stage0.setVisibility(View.VISIBLE);
+            notice1.setVisibility(View.INVISIBLE);
+            notice2.setVisibility(View.INVISIBLE);
+        }
+        if (userScore >= 6) {
+            medal2.setVisibility(View.VISIBLE);
+            stage1.setVisibility(View.VISIBLE);
+        }
+        if (userScore >= 9) {
+            medal3.setVisibility(View.VISIBLE);
+            stage2.setVisibility(View.VISIBLE);
+        }
+        if (userScore >= 12) {
+            medal4.setVisibility(View.VISIBLE);
+            stage3.setVisibility(View.VISIBLE);
+        }
 
         //if finish one lesson and game score > 75marks --> imageView become visible
         //medal1.setVisibility(View.VISIBLE)
